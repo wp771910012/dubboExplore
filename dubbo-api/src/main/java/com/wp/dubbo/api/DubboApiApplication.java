bd68e76dfc2727b4c4d8016a7937c602b752d793
王鹏 <771910012@qq.com>
2018-12-07 11:51:23 +0800

    first commit

---
+++ dubbo-api/src/main/java/com/wp/dubbo/api/DubboApiApplication.java
@@ -0,0 +1,16 @@
+package com.wp.dubbo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wp
+ * @date 2018-11-22 18:09
+ **/
+@SpringBootApplication
+public class DubboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboApiApplication.class,args);
+    }
+}
